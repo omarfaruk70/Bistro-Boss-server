af43c5a7a621a99eb841dccfb6d1c58d7aafdade
omarfaruk70 <faruk1611281892@gmail.com>
2024-01-22 00:09:51 +0600

    added getallCard for count the number and added deleteitem for removing the item card

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(cors());    
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.f3vnw1n.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -44,6 +44,13 @@ async function run() {
         res.send(menucollection);
     })
 
+    // get cart collection number for showing the cart/wishilist button
+    app.get('/getallCard', async(req, res) => {
+      const email = req.query.email;
+      const query = {email: email}
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    })
     // post cart info 
     app.post('/addToCard', async(req, res) => {
       const cartInfo = req.body;
@@ -51,6 +58,13 @@ async function run() {
       res.send(result);
     })
 
+    // delete a item from user dashboard (mycart route)
+    app.delete('/deleteitemfromMycart/:id', async(req, res) => {
+      const item = req.params.id;
+      const query = {_id: new ObjectId(item)};
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+    })
 
 
 
